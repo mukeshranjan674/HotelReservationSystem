f73b670cdd16fb123634236b618300766621f9a6
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-16 19:43:35 +0530

    [Mukesh] add : Ability to show cheapest hotels including weekends

--- src/main/java/com/capgemini/hotelreservationsystem/HotelReservation.java
+++ src/main/java/com/capgemini/hotelreservationsystem/HotelReservation.java
@@ -1,11 +1,12 @@
 package com.capgemini.hotelreservationsystem;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +25,7 @@ public class HotelReservation {
 	 * @param hotel
 	 */
 	public void add(Hotel hotel) {
-		this.hotels.add(hotel);
+		hotels.add(hotel);
 	}
 
 	public List<Hotel> getHotelList() {
@@ -38,23 +39,30 @@ public class HotelReservation {
 	 * @param date2
 	 * @return
 	 */
-	public Map<Integer, Hotel> searchFor(String date1, String date2) {
+	public Map<Hotel, Integer> searchFor(String date1, String date2) {
 		int totalDays = countTotalDays(date1, date2);
 		int weekDays = countWeekDays(date1, date2);
 		int weekendDays = totalDays - weekDays;
 		return getCheapestHotels(weekDays, weekendDays);
 	}
 
-	public Map<Integer, Hotel> getCheapestHotels(int weekDays, int weekendDays) {
-		Map<Integer, Hotel> hotelCosts = new HashMap<>();
-		Map<Integer, Hotel> sortedHotelCosts = new HashMap<>();
+	/**
+	 * UC3
+	 * 
+	 * @param weekDays
+	 * @param weekendDays
+	 * @return
+	 */
+	public Map<Hotel, Integer> getCheapestHotels(int weekDays, int weekendDays) {
+		Map<Hotel, Integer> hotelCosts = new HashMap<>();
+		Map<Hotel, Integer> sortedHotelCosts = new HashMap<>();
 		if (hotels.size() == 0)
 			return null;
-		this.hotels.stream().forEach(
-				n -> hotelCosts.put(n.getRegularWeekdayRate() * weekDays + n.getRegularWeekendRate() * weekendDays, n));
-		Integer cheap = hotelCosts.keySet().stream().min(Integer::compare).get();
+		hotels.stream().forEach(
+				n -> hotelCosts.put(n, n.getRegularWeekdayRate() * weekDays + n.getRegularWeekendRate() * weekendDays));
+		Integer cheap = hotelCosts.values().stream().min(Integer::compare).get();
 		hotelCosts.forEach((k, v) -> {
-			if (k == cheap)
+			if (v.equals(cheap))
 				sortedHotelCosts.put(k, v);
 		});
 		return sortedHotelCosts;
@@ -68,20 +76,33 @@ public class HotelReservation {
 		return totalDays;
 	}
 
+	/**
+	 * UC4
+	 * 
+	 * @param date1
+	 * @param date2
+	 * @return
+	 */
 	public int countWeekDays(String date1, String date2) {
 
 		LocalDate startDate = toLocalDate(date1);
 		LocalDate endDate = toLocalDate(date2);
+		Date startDay = Date.from(startDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		Date endDay = Date.from(endDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
 
-		DayOfWeek startDay = startDate.getDayOfWeek();
-		DayOfWeek endDay = endDate.getDayOfWeek();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(startDay);
 
-		long days = ChronoUnit.DAYS.between(startDate, endDate) + 1;
-		long daysWithoutWeekends = days - 2 * ((days + startDay.getValue()) / 7);
-		int totalWeekDays = (int) daysWithoutWeekends + (startDay == DayOfWeek.SUNDAY ? 1 : 0)
-				+ (endDay == DayOfWeek.SUNDAY ? 1 : 0);
+		int weekDays = 0;
+		while (!calendar.getTime().after(endDay)) {
+			int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+			if ((dayOfWeek > 1) && (dayOfWeek < 7)) {
+				weekDays++;
+			}
+			calendar.add(Calendar.DAY_OF_MONTH, 1);
+		}
 
-		return totalWeekDays;
+		return weekDays;
 	}
 
 	public LocalDate toLocalDate(String date) {
--- src/test/java/com/capgemini/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/capgemini/hotelreservationsystem/HotelReservationTest.java
@@ -42,8 +42,8 @@ public class HotelReservationTest {
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
 		hotelReservation.add(hotel3);
-		Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
-		result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
+		Map<Hotel, Integer> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+		result.forEach((k, v) -> System.out.println(k.getName() + " " + v));
 		assertNotNull(result);
 	}
 
@@ -60,9 +60,25 @@ public class HotelReservationTest {
 		hotelReservation.add(hotel2);
 		hotelReservation.add(hotel3);
 		List<Hotel> hotelList = hotelReservation.getHotelList();
-		boolean result = hotelList.get(0).getRegularWeekendRate() == 90 && 
-						 hotelList.get(1).getRegularWeekendRate() == 60 && 
-						 hotelList.get(2).getRegularWeekendRate() == 150;
+		boolean result = hotelList.get(0).getRegularWeekendRate() == 90
+				&& hotelList.get(1).getRegularWeekendRate() == 60 && hotelList.get(2).getRegularWeekendRate() == 150;
 		assertTrue(result);
+	}
+
+	/**
+	 * UC4
+	 */
+	@Test
+	public void whenGivenDateRangeShouldReturnCheapestHotels() {
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		Map<Hotel, Integer> result = hotelReservation.searchFor("11Sep2020", "12Sep2020");
+		result.forEach((k, v) -> System.out.println(k.getName() + " " + v));
+		assertNotNull(result);
 	}
 }
