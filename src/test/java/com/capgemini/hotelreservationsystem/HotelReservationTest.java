58a378ff0179c58a9fbfd2da03001b60cb62a121
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-16 23:33:11 +0530

    [Mukesh] add : Ability to search for best rated hotels

--- src/test/java/com/capgemini/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/capgemini/hotelreservationsystem/HotelReservationTest.java
@@ -42,7 +42,7 @@ public class HotelReservationTest {
 		hotelReservation.add(hotel1);
 		hotelReservation.add(hotel2);
 		hotelReservation.add(hotel3);
-		Map<Hotel, Integer> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+		Map<Hotel, Integer> result = hotelReservation.getCheapestHotels("10Sep2020", "11Sep2020");
 		result.forEach((k, v) -> System.out.println(k.getName() + " " + v));
 		assertNotNull(result);
 	}
@@ -61,8 +61,7 @@ public class HotelReservationTest {
 		hotelReservation.add(hotel3);
 		List<Hotel> hotelList = hotelReservation.getHotelList();
 		boolean result = hotelList.get(0).getRegularWeekendRate() == 90
-				&& hotelList.get(1).getRegularWeekendRate() == 60 
-				&& hotelList.get(2).getRegularWeekendRate() == 150;
+				&& hotelList.get(1).getRegularWeekendRate() == 60 && hotelList.get(2).getRegularWeekendRate() == 150;
 		assertTrue(result);
 	}
 
@@ -114,6 +113,24 @@ public class HotelReservationTest {
 		hotelReservation.add(hotel2);
 		hotelReservation.add(hotel3);
 		Map<Hotel, Integer> result = hotelReservation.getCheapestAndBestRatedHotels("11Sep2020", "12Sep2020");
+		result.forEach(
+				(k, v) -> System.out.println(k.getName() + ", Rating : " + k.getRating() + " and Total Rate " + v));
+		assertNotNull(result);
+	}
+
+	/**
+	 * UC7
+	 */
+	@Test
+	public void whenGivenDateRangeShouldReturnBestRatedHotels() {
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 110, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.add(hotel1);
+		hotelReservation.add(hotel2);
+		hotelReservation.add(hotel3);
+		Map<Hotel, Integer> result = hotelReservation.getBestRatedHotels("11Sep2020", "12Sep2020");
 		result.forEach(
 				(k, v) -> System.out.println(k.getName() + ", Rating : " + k.getRating() + " and Total Rate " + v));
 		assertNotNull(result);
